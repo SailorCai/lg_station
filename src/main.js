5462f8319dc1948a4de498c57b1f25c6bee408e7
logan <1187023819@qq.com>
2018-02-02 18:40:21 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import { Cell, Checklist } from 'mint-ui'
+import router from './router'
+Vue.component(Cell.name, Cell)
+Vue.component(Checklist.name, Checklist)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
