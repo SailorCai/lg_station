7c0d2163674e6325505ec46b87adfcd1fb98d998
logan <1187023819@qq.com>
2018-02-05 12:08:40 +0800

    add iconfont

---
+++ src/components/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scope>
+  .main{
+    width:100%;
+    height:auto;
+    padding-top:160px;
+  }
+</style>
