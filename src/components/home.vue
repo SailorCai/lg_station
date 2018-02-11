3afb38ef434bfe49216eca09b8a7292134804177
logan <1187023819@qq.com>
2018-02-11 18:16:13 +0800

    5555

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,18 +1,27 @@
 <template>
 <section>
-  <mt-popup position="top" :modal="false" v-model="popupVisible" style="width:100%;background:none;">
+  <mt-popup position="top" :modal="false" v-model="popupTopVisible" style="width:100%;background:none;">
     <div class="search_modal">
       <div class="search_wrapper">
         <input type="text" class="input_text">
         <div class="search_btn">搜索</div>
-        <div class="search_btn bg_danger" @click="popupVisible=false">取消</div>
+        <div class="search_btn bg_danger" @click="popupTopVisible=false">取消</div>
+      </div>
+    </div>
+  </mt-popup>
+  <mt-popup position="left" :modal="true" v-model="popupLeftVisible" style="width:70%;background:#fff;max-width:300px;height:100%;">
+    <div class="aside_modal">
+      <div class="aside_head">
+      <div class="menu_btn menu_btn_aside" @click="popupLeftVisible=!popupLeftVisible"><i class="iconfont icon-fanhui"></i></div>
+        <div class="aside_avatar"><img src="http://img1.imgtn.bdimg.com/it/u=1062502429,3132037224&fm=200&gp=0.jpg"></div>
+        <div class="aside_title">蔡胜龙的博客</div>
       </div>
     </div>
   </mt-popup>
   <div class="main">
     <!-- 菜单按钮 -->
-    <div class="menu_btn menu_btn_left"><i class="iconfont icon-liebiao"></i></div>
-    <div class="menu_btn menu_btn_right" @click="popupVisible=!popupVisible"><i class="iconfont icon-sousuosearch79"></i></div>
+    <div class="menu_btn menu_btn_left" @click="popupLeftVisible=!popupLeftVisible"><i class="iconfont icon-liebiao"></i></div>
+    <div class="menu_btn menu_btn_right" @click="popupTopVisible=!popupTopVisible"><i class="iconfont icon-sousuosearch79"></i></div>
     <!-- 主体内容 -->
     <div class="content_panel">
       <div class="author_pan blog_item">
@@ -49,7 +58,8 @@ export default {
   name: 'App',
   data() {
     return {
-      popupVisible:false,
+      popupTopVisible:false,
+      popupLeftVisible:false,
     }
   },
   components: {
@@ -184,4 +194,41 @@ export default {
     margin:0 2px;
   }
   .bg_danger{background-color:#F56C6C !important;}
+  .aside_modal{
+    width:100%;
+    background:#fff;
+    min-height:100%;
+    display:flex;
+    flex-direction: column;
+  }
+  .aside_head{
+    display:flex;
+    flex-direction: column;
+    height:12rem;
+    background:#333;
+    box-sizing:border-box;
+    padding-top:30px;
+  }
+  .aside_avatar{
+    padding:0 1rem;
+    flex-grow:1;
+    display:flex;
+    flex-direction: column;
+    justify-content: flex-end;
+  }
+  .aside_avatar img{
+    border-radius:50%;
+    width:6rem;
+    height:6rem;
+  }
+  .aside_title{
+    padding:0.5rem 1rem;
+    font-size:0.7rem;
+    color:#fff;
+  }
+  .menu_btn_aside{
+    top:4rem;
+    right:0.5rem;
+  }
+  .menu_btn_aside .iconfont{color:#fff;}
 </style>
