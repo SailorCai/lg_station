3eb22c2721d1ef620d6e14333278b82937139639
logan <1187023819@qq.com>
2018-02-24 12:33:52 +0800

    add ecosystem.json

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('dist'));
+
+app.get('/', function(req, res){
+  res.redirect('/index.shtml');
+  //res.send('hello world');
+});
+
+var server = app.listen(3003, function(){
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('应用实例，访问地址为http://%s%s', host, port);
+});
