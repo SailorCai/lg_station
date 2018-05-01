c3be177aef37763b2d05a6f7d3ee8c7e2ab1cef8
logan <1187023819@qq.com>
2018-05-01 21:22:44 +0800

    hahah

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ var env = process.env.NODE_ENV || 'development';
 app.use(express.static(path.join(__dirname, 'dist')));
 var dbUrl = 'mongodb://my_runner:J**k9001$@127.0.0.1:29999/lg_data';
 
-if(env === 'development'){
+/*if(env === 'development'){
   dbUrl = 'mongodb://localhost/lgData';
-};
+};*/
 
 mongoose.connect(dbUrl, {
   useMongoClient:true
